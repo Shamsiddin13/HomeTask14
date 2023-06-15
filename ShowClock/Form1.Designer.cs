d5fb328cef11e3b4aa175cd4908c14b56b31181b
Shamsiddin_Dev <shamsiddinumarov0013@gmail.com>
2023-06-15 17:45:30 +0500

    Add project files.

---
+++ ShowClock/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ShowClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelStatus.ForeColor = System.Drawing.Color.Green;
+            this.LabelStatus.Location = new System.Drawing.Point(40, 23);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(270, 69);
+            this.LabelStatus.TabIndex = 0;
+            this.LabelStatus.Text = "00:00:00";
+            this.LabelStatus.Click += new System.EventHandler(this.LabelStatus_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 114);
+            this.Controls.Add(this.LabelStatus);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ShowClock/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LabelStatus.Text = DateTime.Now.ToString("T");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer1.Start();
+            System.Timers.Timer timer = new System.Timers.Timer();  
+            timer.Interval = 1000;
+        }
+
+        private void LabelStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
